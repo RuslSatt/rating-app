b12648d52eaf3e6e1cebbbfe9d08929d259aea65
RuslSatt <r8sattarov@mail.ru>
2025-09-02 20:31:30 +0500

    chore: update font settings to use Noto Sans Mono and adjust global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Noto_Sans_Mono } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
+const notoSansMono = Noto_Sans_Mono({
+  variable: '--font-noto-sans-mono',
+  subsets: ['latin', 'cyrillic']
 });
 
 export const metadata: Metadata = {
@@ -24,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ru'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+      <body className={`${notoSansMono.variable}`}>{children}</body>
     </html>
   );
 }
