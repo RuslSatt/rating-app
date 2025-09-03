db4d712321775ceb34418802b5302265ea50afb7
RuslSatt <r8sattarov@mail.ru>
2025-09-03 23:53:36 +0500

    feat: enhance HTag component to support additional HTML attributes

--- src/components/Htag/HTag.tsx
+++ src/components/Htag/HTag.tsx
@@ -1,12 +1,16 @@
 import styles from './HTag.module.css';
 
-export interface HTagProps {
+export interface HTagProps extends React.HTMLAttributes<HTMLHeadingElement> {
   Tag: 'h1' | 'h2' | 'h3';
   children: React.ReactNode;
 }
 
 export const HTag: React.FC<HTagProps> = (props) => {
-  const { Tag, children } = props;
+  const { Tag, children, ...otherProps } = props;
 
-  return <Tag className={styles[Tag]}>{children}</Tag>;
+  return (
+    <Tag className={styles[Tag]} {...otherProps}>
+      {children}
+    </Tag>
+  );
 };
